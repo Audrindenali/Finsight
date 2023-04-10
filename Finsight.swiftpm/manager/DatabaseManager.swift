23b0722becfec7d3e6670f58384d07db8d9ea603
rivaldofez <rivaldofez@gmail.com>
2023-04-10 12:50:01 +0700

    add save transcation function in database manager

--- Finsight.swiftpm/manager/DatabaseManager.swift
+++ Finsight.swiftpm/manager/DatabaseManager.swift
@@ -8,8 +8,36 @@
 import Foundation
 import RealmSwift
 
+//@Persisted(primaryKey: true) var _id: String
+//@Persisted var tr_category: String = ""
+//@Persisted var tr_amount: Double = 0
+//@Persisted var tr_date: Date = Date()
+//@Persisted var tr_description : String = ""
+
 class DatabaseManager {
     static let shared = DatabaseManager()
     
-    
+    func saveTransaction(
+        tr_category: String,
+        tr_amount: Double,
+        tr_date: Date,
+        tr_description: String
+    ){
+        let realm = try! Realm()
+        
+        let transaction = TransactionEntity()
+        transaction._id = UUID().uuidString
+        transaction.tr_category = tr_category
+        transaction.tr_amount = tr_amount
+        transaction.tr_date = tr_date
+        transaction.tr_description = tr_description
+        
+        do {
+            try realm.write {
+                realm.add(transaction)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
 }
