90f6bb5465d0728ff86c0c25a0d7422c9cdb232d
rivaldofez <rivaldofez@gmail.com>
2023-04-10 12:44:18 +0700

    add transaction entity

---
+++ Finsight.swiftpm/Model/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 10/04/23.
+//
+
+import Foundation
+import RealmSwift
+
+class TransactionEntity: Object {
+    @Persisted(primaryKey: true) var _id: String
+    @Persisted var tr_category: String = ""
+    @Persisted var tr_amount: Double = 0
+    @Persisted var tr_date: Date = Date()
+    @Persisted var tr_description : String = ""
+}
---
+++ Finsight.swiftpm/manager/DatabaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 10/04/23.
+//
+
+import Foundation
+import RealmSwift
+
+class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    
+}
