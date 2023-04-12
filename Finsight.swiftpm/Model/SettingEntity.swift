ff004c66ffa7a47668164216e10406e5a216dfbb
rivaldofez <rivaldofez@gmail.com>
2023-04-12 09:53:47 +0700

    implement profile page

---
+++ Finsight.swiftpm/Model/SettingEntity.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 12/04/23.
+//
+
+import Foundation
+
+struct SettingEntity {
+    var image: String
+    var name: String
+}
+
--- Finsight.swiftpm/ProfileView.swift
+++ Finsight.swiftpm/ProfileView.swift
@@ -8,23 +8,82 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    var settingList = [
+        SettingEntity(image: "person", name: "Account"),
+        SettingEntity(image: "person", name: "Settings"),
+        SettingEntity(image: "person", name: "Export Data"),
+        SettingEntity(image: "person", name: "Logout"),
+    ]
+    
     var body: some View {
-        VStack {
-            HStack {
-                Image("welcome_background")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
-                
-                VStack(alignment: .leading) {
-                    Text("Username")
-                    Text("Rebecca")
+        ZStack {
+            Color.mainColor
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Image("welcome_background")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading) {
+                        Text("Username")
+                            .foregroundColor(.mainText)
+                            
+                        Text("Rebecca")
+                            .foregroundColor(.mainText)
+                            .font(.system(.title))
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    Image(systemName: "person")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .padding(12)
+                        .foregroundColor(.white)
+                        .font(.system(.title).bold())
+//                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.white)
+                        }
                 }
+                .padding(.horizontal)
                 
-                Image("welcome_background")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                VStack {
+                    ForEach(0..<settingList.count, id: \.self) { stIndex in
+                        HStack {
+                            
+                            Image(systemName: settingList[stIndex].image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 20)
+                                .padding(18)
+                                .foregroundColor(.mainColor)
+                                .font(.system(.title).bold())
+                                .background(Color.mainColor.opacity(0.2))
+                                .clipShape(RoundedRectangle(cornerRadius: 16))
+                            
+                            Text(settingList[stIndex].name)
+                                .foregroundColor(.black)
+                                .font(.system(.body).bold())
+                            
+                            Spacer()
+                            
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical)
+                    }
+                }
+                .padding(.all)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 24))
+                .padding(.all)
             }
         }
     }
